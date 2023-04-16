1b944ef9fbbd7246ad67a2636e7433d69d518afc
RazaNaqsh <owaisraza1704@gmail.com>
2023-04-16 19:19:02 +0530

    Routes and controller for CUD actions

--- api/controllers/blogController.js
+++ api/controllers/blogController.js
@@ -1,11 +1,40 @@
 const Blog = require("../models/blogModel");
 const mongoose = require("mongoose");
 
+// get all blogs
 const get_blogs = async (req, res) => {
 	const blogs = await Blog.find({}).sort({ createdAt: -1 });
 	res.status(200).json(blogs);
 };
 
+// create blog
+const create_blogs = async (req, res) => {
+	const { title, snippet, content } = req.body;
+	const blog = await Blog.create({
+		title,
+		snippet,
+		content,
+	});
+	res.status(200).json(blog);
+};
+
+// get a single blog
+const get_blog = async (req, res) => {
+	const { id } = req.params;
+	const blog = await Blog.findById(id);
+	res.status(200).json(blog);
+};
+
+// delete a blog
+const delete_blog = async (req, res) => {
+	const { id } = req.params;
+	const blog = await Blog.findOneAndDelete({ _id: id });
+	res.status(200).json(blog);
+};
+
 module.exports = {
 	get_blogs,
+	create_blogs,
+	get_blog,
+	delete_blog,
 };
--- api/routes/blogRoutes.js
+++ api/routes/blogRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 
-const { get_blogs } = require("../controllers/blogController");
+const {
+	get_blogs,
+	create_blogs,
+	get_blog,
+	delete_blog,
+} = require("../controllers/blogController");
 
 const router = express.Router();
 
 router.get("/", get_blogs);
+router.post("/", create_blogs);
+router.get("/:id", get_blog);
+router.delete("/:id", delete_blog);
 
 module.exports = router;
