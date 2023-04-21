de86ec18d1eec6ec318c9f0667465f0e51316738
RazaNaqsh <owaisraza1704@gmail.com>
2023-04-21 23:01:00 +0530

    can delete blogs

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -2,8 +2,11 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import { format } from "date-fns";
+import { useNavigate } from "react-router-dom";
 
 const Blog = () => {
+	const navigate = useNavigate();
+
 	const [blogs, setBlogs] = useState([]);
 
 	useEffect(() => {
@@ -23,6 +26,18 @@ const Blog = () => {
 	useEffect(() => {
 		console.log(blogs);
 	}, [blogs]);
+
+	const handleDelete = async (id, e) => {
+		e.stopPropagation();
+		console.log("this is id", id);
+		const response = await fetch("http://localhost:4000/api/blogs/" + id, {
+			method: "DELETE",
+		});
+		const json = await response.json();
+		if (response.ok) {
+			navigate("/");
+		}
+	};
 	return (
 		<section>
 			<div className="py-10">
@@ -30,13 +45,19 @@ const Blog = () => {
 					Read ideas from across the World..
 				</h3>
 				<div className="my-10 w-[90%] mx-auto flex flex-wrap  gap-8 ">
-					{blogs &&
+					{blogs.length > 0 ? (
 						blogs.map((blog) => (
 							<Link
 								key={blog._id}
 								to={`/blogs/${blog._id}`}
 							>
-								<div className="w-[300px] h-[300px] p-6 border-2 rounded-lg border-indigo-300 space-y-2">
+								<div className="w-[300px] h-[300px] p-6 border-2 rounded-lg border-indigo-300 space-y-2 relative z-10">
+									<span
+										className="absolute z-50 top-3 right-3 material-symbols-outlined hover:scale-90"
+										onClick={() => handleDelete(blog._id, event)}
+									>
+										delete
+									</span>
 									<h3 className="text-lg ">{blog.title}</h3>
 									<p className="text-sm">{blog.snippet}</p>
 									{/* <p className="text-sm text-right">
@@ -50,7 +71,14 @@ const Blog = () => {
 									</p>
 								</div>
 							</Link>
-						))}
+						))
+					) : (
+						<div className="m-auto">
+							<p className="text-3xl text-white border-2 border-indi ">
+								No Blogs to Read
+							</p>
+						</div>
+					)}
 				</div>
 			</div>
 		</section>
